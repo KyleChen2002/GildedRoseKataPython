e06d2ab56e89125e98567cdf820207239ca3aa05
Junkai Chen <chen.junka@northeastern.edu>
2025-02-05 19:27:50 -0800

    upload lab4 file

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,15 +1,29 @@
-# -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_general_item_quality_decreases(self):
+        items = [Item("+5 Dexterity Vest", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(items[0].quality, 21)  
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, -1) 
+    def test_backstage_passes_quality_drops_to_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 50) 
+
+    def test_gilded_rose_missing_method(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.get_items()  
+
 
 
 if __name__ == '__main__':
